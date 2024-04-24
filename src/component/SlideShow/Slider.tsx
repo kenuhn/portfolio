5711396ac92fa089e735563e7275ea3ce8263f3d
kenuhn Rimbert <kenuhn@gmail.com>
2024-04-24 19:45:03 +0200

    Add chip8

--- src/component/SlideShow/Slider.tsx
+++ src/component/SlideShow/Slider.tsx
@@ -1,54 +1,55 @@
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
-import Card from '../Card/Card';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import data from "../../../public/data/data.json"
+import data from "../../../public/data/data.json";
+import Card from "../Card/Card";
 
 // import required modules
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
-
+import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
 
 const Slider = () => {
-const projects = data
+  const projects = data;
 
-    return (
-        <section className="swiper_contenant" id="projects" >
-
-            <h3 className="slider_title"> MES PROJETS </h3>
-            <Swiper
-                effect={'coverflow'}
-                grabCursor={false}
-                centeredSlides={true}
-                /* loop={true}  */
-                slidesPerView={'auto'}
-                coverflowEffect={{
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 2.5,
-                }}
-                pagination={{ el: '.swiper-pagination', clickable: true }}
-                navigation
-                initialSlide={5}
-                modules={[EffectCoverflow, Pagination, Navigation]}
-                className="swiper_container">
-
-
-                {projects.map((project) => {
-                    return (
-                        <SwiperSlide key={project.name} className="card_content" >
-                            <Card key={project.name} titre={project.name} img={project.imgProject} description={project.descriptionCard} id={project.id} tags={project.techno} year={project.year}/>
-                        </SwiperSlide>
-                    )
-                })}
-
-
-            </Swiper>
-        </section>
-
-
-    );
-}
+  return (
+    <section className="swiper_contenant" id="projects">
+      <h3 className="slider_title"> MES PROJETS </h3>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={false}
+        centeredSlides={true}
+        /* loop={true}  */
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
+        navigation
+        initialSlide={4}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        {projects.map((project) => {
+          return (
+            <SwiperSlide key={project.name} className="card_content">
+              <Card
+                key={project.name}
+                titre={project.name}
+                img={project.imgProject}
+                description={project.descriptionCard}
+                id={project.id}
+                tags={project.techno}
+                year={project.year}
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
 export default Slider;
--- src/component/Timeline/Timeline.tsx
+++ src/component/Timeline/Timeline.tsx
@@ -1,4 +1,7 @@
+
 const Timeline = () => {
+    
+
 
     return (
 
